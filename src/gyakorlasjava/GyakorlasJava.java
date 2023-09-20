81bd80f4092cded08823ccf85d2d0b7bc15d8fce
Fűrész Bence (SZF_2022_2024) <furesz.bence@diak.szamalk-szalezi.hu>
2023-09-20 12:50:02 +0200

    valtoztatas

--- src/gyakorlasjava/GyakorlasJava.java
+++ src/gyakorlasjava/GyakorlasJava.java
@@ -1,20 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gyakorlasjava;
 
-/**
- *
- * @author FűrészBence(SZF_2022
- */
 public class GyakorlasJava {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+      int a;
     }
     
 }
